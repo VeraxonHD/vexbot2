b11f85b86a69c1270d9e1f185124918727eddae9
VeraxonHD <toxicscopesgames@gmail.com>
2017-04-01 15:33:09 +0100

    Additional functionality and dress-up for quote

--- commands/quote.js
+++ commands/quote.js
@@ -1,14 +1,16 @@
 exports.run = (client, message, args) => {
   var messageid = args[0]
+  var guild = message.guild
   const Discord = require("discord.js");
   message.channel.fetchMessages({
       around: messageid,
       limit: 1
     })
     .then((quote) => {
+      var guildmember = guild.members.get(quote.first().author.id).nickname ? guild.members.get(quote.first().author.id).nickname : quote.first().author.username
       var embed = new Discord.RichEmbed()
         .setColor(0x00AE86)
-        .setAuthor(quote.first().author.username)
+        .setAuthor(`${guildmember} (AKA ${quote.first().author.username}#${quote.first().author.discriminator})`)
         .setDescription(quote.first().content)
         .setTimestamp(quote.first().createdAt);
       message.delete()
